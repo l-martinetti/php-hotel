f6da974903ebcb5b755f1ca48df1f7f389c3ae65
l-martinetti <luca.martinetti@outlook.it>
2025-03-24 23:22:55 +0100

    feat: add hotels table

--- index.php
+++ index.php
@@ -51,6 +51,7 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
     
     <title>PHP Hotel</title>
+
 </head>
 <body>
 
@@ -58,14 +59,42 @@
 
 <?php
 
-foreach($hotels as $hotel){
-    foreach($hotel as $key => $value){
-        echo "$key: $value <br>";
-    }
-
-    echo "<br>";
-}
 ?>
+
+<table class="table">
+  <thead>
+    <tr>
+        <?php 
+
+        foreach($hotels[0] as $key => $value) {
+            $key = ucfirst($key);
+            echo "<th scope='col'>$key</th>";
+        }
+
+      ?>
+    </tr>
+  </thead>
+  <tbody>
+            <?php
+
+        foreach($hotels as $hotel){
+            
+            foreach($hotel as $key => $value){
+                
+                if ($key == 'parking' && $value == true){
+                    echo "<td>Si</td>";
+                } elseif ($key == 'parking' && $value == false) {
+                    echo "<td>No</td>";
+                } else {
+                    echo "<td>$value</td>";
+                }
+            }
+
+           echo "<tr></tr>";  
+        }
+            ?>
+  </tbody>
+</table>
     
 </body>
 </html>
