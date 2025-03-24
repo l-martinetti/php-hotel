7392aa09ca33813b27f11933ffa30edb9983f856
l-martinetti <luca.martinetti@outlook.it>
2025-03-24 22:15:48 +0100

    feat: add bootstrap cdn

--- index.php
+++ index.php
@@ -46,6 +46,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+    
     <title>PHP Hotel</title>
 </head>
 <body>
